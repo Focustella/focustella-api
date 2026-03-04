3d63e1ace9165039cfac2fed5b8445e7a7a2443a
DongHyun KIM <105835957+Dxdchd@users.noreply.github.com>
2026-03-04 22:59:24 +0900

    chore : init setting

---
+++ src/main/java/com/example/focustellaapi/FocustellaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.focustellaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocustellaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FocustellaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/focustellaapi/FocustellaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.focustellaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocustellaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
